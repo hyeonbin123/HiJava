1e89aba4c5764dcb925959407729080edbf0fcfb
USER <USER@DESKTOP-I734LTD>
2021-07-13 10:14:15 +0900

    첫번째 깃소스

---
+++ HiJava/src/S_20517_0713/GitTest.java
@@ -0,0 +1,10 @@
+package S_20517_0713;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¾È³ç ±êÇãºê Ã¹¹øÂ°");
+	}
+
+}
